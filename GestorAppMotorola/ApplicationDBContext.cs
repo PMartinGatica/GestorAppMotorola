a28d6ef9dd2aa04f16daf90c5b5e8d704c9a82b2
Pablo <pablomartin.gatica@gmail.com>
2021-11-29 19:45:34 -0300

    Creacion de ClaseDBContext

---
+++ GestorAppMotorola/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using API_Gestion_Instalacion_APP.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gestion_Instalacion_APP
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Operarios> Operarios { get; set; }
+        public DbSet<Apps> Apps { get; set; }
+        public DbSet<Instalaciones> Instalaciones { get; set; }
+
+
+    }
+}
